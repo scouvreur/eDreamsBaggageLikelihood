ba6087e4aa12c21334b54f7467e7ba26573cde6d
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2018-05-10 22:17:17 +0100

    XGBoost model created and more feature engineering with FAMILY_SIZE variable, also more graphs for the report

---
+++ boosted.py
@@ -0,0 +1,69 @@
+"""
+================================================
+
+"All models are wrong, but some are useful."
+George Box, 1976
+
+================================================
+"""
+
+print(__doc__)
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from xgboost import XGBClassifier
+
+test = pd.read_csv("test_xgboost.csv", sep = ",")
+train = pd.read_csv("train_xgboost.csv", sep = ",")
+
+categorical_features = ["HAUL_TYPE","DEVICE","TRIP_TYPE","COMPANY"]
+
+train["COMPANY"] = LabelEncoder().fit_transform(train["COMPANY"].astype("str"))
+train["HAUL_TYPE"] = LabelEncoder().fit_transform(train["HAUL_TYPE"].astype("str"))
+train["TRIP_TYPE"] = LabelEncoder().fit_transform(train["TRIP_TYPE"].astype("str"))
+train["DEVICE"] = LabelEncoder().fit_transform(train["DEVICE"].astype("str"))
+train["DISTANCE"] = train["DISTANCE"].astype("float")
+train["EXTRA_BAGGAGE"] = LabelEncoder().fit_transform(train["EXTRA_BAGGAGE"].astype("str"))
+
+test["COMPANY"] = LabelEncoder().fit_transform(test["COMPANY"].astype("str"))
+test["HAUL_TYPE"] = LabelEncoder().fit_transform(test["HAUL_TYPE"].astype("str"))
+test["TRIP_TYPE"] = LabelEncoder().fit_transform(test["TRIP_TYPE"].astype("str"))
+test["DEVICE"] = LabelEncoder().fit_transform(test["DEVICE"].astype("str"))
+test["DISTANCE"] = test["DISTANCE"].astype("float")
+
+features = ["FAMILY_SIZE","DISTANCE","HAUL_TYPE","DEVICE","TRIP_TYPE","COMPANY"]
+
+X_train = train[list(features)].values
+Y_train = train["EXTRA_BAGGAGE"].values
+X_test = test[list(features)].values
+
+X_train, X_validation, Y_train, Y_validation = train_test_split(X_train, Y_train,
+																test_size=0.2,
+																random_state=747)
+
+n_estimators=1000
+max_depth=8
+learning_rate=1.0
+
+# clf = XGBClassifier()
+clf = XGBClassifier(n_estimators=n_estimators, max_depth=max_depth, learning_rate=learning_rate)
+clf.fit(X_train, Y_train)
+
+Y_test = clf.predict_proba(X_test)
+
+print("--- Model parameters ---")
+print("XGBClassifier(n_estimators={}, max_depth={}, learning_rate={})".format(n_estimators, max_depth, learning_rate))
+
+print("--- Validation set ---")
+print("AUC;{}".format(roc_auc_score(Y_validation, clf.predict(X_validation))))
+
+print("--- Training set ---")
+print("AUC;{}".format(roc_auc_score(Y_train, clf.predict(X_train))))
+
+# print("ID;EXTRA_BAGGAGE")
+# for i in range(len(X_test)):
+#     print("{};{:.6f}".format(i,Y_test[i,1]))
--- model.R
+++ model.R
@@ -9,15 +9,31 @@ cat("\014")
 setwd("~/Dropbox/Documents/Projects/DataScience/eDreamsBaggageLikelihood")
 
 # Load libraries
-library(haven, lmtest)
+library(pROC)
 
 # Load in data
 train <- read.csv("train.csv", header = TRUE, sep = ";", na.strings=c(""," ","NA"), stringsAsFactors = TRUE)
 test <- read.csv("test.csv", header = TRUE, sep = ";", na.strings=c(""," ","NA"), stringsAsFactors = TRUE)
 
+# Check for missing data
+# length(train[!complete.cases(train),])
+
+# Add a labels to the categories of variables
+train$EXTRA_BAGGAGE <- factor(train$EXTRA_BAGGAGE, levels = c("False","True"), labels = c("No Extra Baggage", "Extra Baggage"))
+
+# Reformatting data structure types
+str(train)
+str(test)
+train$DISTANCE<- as.numeric(train$DISTANCE)
+test$DISTANCE <- as.numeric(test$DISTANCE)
+
 ## Preliminary data investigation
+# Severe class imbalance with these two variables
 round(prop.table(table(train$TRAIN))*100, digits = 1)
 round(prop.table(table(train$PRODUCT))*100, digits = 1)
+
+# It seems that there is not much interesting with SMS confirmation
+round(prop.table(table(train$SMS, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
 
 # Create a utility function to help with website extraction using UNIX grep
 extractWebsite <- function(name) {
@@ -29,7 +45,7 @@ extractWebsite <- function(name) {
   } else if (length(grep("GO", name)) > 0) {
     return("GO VOYAGE")
   } else {
-    return("Other")
+    return("OTHER")
   }
 }
 
@@ -48,33 +64,20 @@ test$COMPANY <- as.factor(COMPANY)
 
 rm(COMPANY, i, extractWebsite)
 
+# Create family size variable
+FAMILY_SIZE = train$ADULTS + train$CHILDREN + train$INFANTS
+train$FAMILY_SIZE <- FAMILY_SIZE
+FAMILY_SIZE = test$ADULTS + test$CHILDREN + test$INFANTS
+test$FAMILY_SIZE <- FAMILY_SIZE
+rm(FAMILY_SIZE)
+
 # Variables not of interest removed
 # Assuming there is no local variability between countries (UK, Italy, Spain etc.), big assumption though...
-train <- subset(train, select = -c(TIMESTAMP, DEPARTURE:ARRIVAL, TRAIN, PRODUCT, GDS, NO_GDS, WEBSITE))
-test <- subset(test, select = -c(TIMESTAMP, DEPARTURE:ARRIVAL, TRAIN, PRODUCT, GDS, NO_GDS, WEBSITE))
+train <- subset(train, select = -c(TIMESTAMP, DEPARTURE:ARRIVAL, TRAIN, PRODUCT, GDS, NO_GDS, WEBSITE, SMS))
+test <- subset(test, select = -c(TIMESTAMP, DEPARTURE:ARRIVAL, TRAIN, PRODUCT, GDS, NO_GDS, WEBSITE, SMS))
 
-# Reformatting data structure for data
-str(train)
-str(test)
-train$DISTANCE<- as.numeric(train$DISTANCE)
-test$DISTANCE <- as.numeric(test$DISTANCE)
-
-# It seems that there is not much interesting between different booking websites, they are all the same
+# There is insteresting subtle variability between different booking websites, not all the same
 round(prop.table(table(train$COMPANY, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
-
-# We therefore remove that website variable
-train <- subset(train, select = -c(COMPANY))
-test <- subset(test, select = -c(COMPANY))
-
-# It seems that there is not much interesting with SMS confirmation or booking device either
-round(prop.table(table(train$SMS, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
-round(prop.table(table(train$DEVICE, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
-
-# We therefore remove that SMS variable
-train <- subset(train, select = -c(SMS, DEVICE))
-test <- subset(test, select = -c(SMS, DEVICE))
-
-# 
 round(prop.table(table(train$HAUL_TYPE, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
 round(prop.table(table(train$TRIP_TYPE, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
 
@@ -83,19 +86,9 @@ round(prop.table(table(train$ADULTS, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
 round(prop.table(table(train$CHILDREN, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
 round(prop.table(table(train$INFANTS, train$EXTRA_BAGGAGE), 1)*100, digits = 1)
 
-# We could create a synthetic variable when at least on parent travels with one child or infant
-extractWebsite <- function(name) {
-  name <- as.character(name)
-  if (length(grep("ED", name)) > 0) {
-    return("EDREAMS")
-  } else if (length(grep("OP", name)) > 0) {
-    return("OPODO")
-  } else if (length(grep("GO", name)) > 0) {
-    return("GO VOYAGE")
-  } else {
-    return("Other")
-  }
-}
+# Export data for Python Machine Learning model
+# write.csv(train, file="train_nana.csv", row.names = FALSE)
+# write.csv(test, file="test_nana.csv", row.names = FALSE)
 
 ## Building the actual model
 
@@ -103,14 +96,22 @@ extractWebsite <- function(name) {
 validation <- train[40001:50000,]
 train <- train[0:40000,]
 
-model1 <- glm(factor(EXTRA_BAGGAGE) ~ DISTANCE + factor(HAUL_TYPE) + factor(TRIP_TYPE) + factor(DEVICE) + factor(SMS) + factor(COMPANY) + ADULTS + CHILDREN + INFANTS,
+model <- glm(EXTRA_BAGGAGE ~ DISTANCE + factor(HAUL_TYPE) + factor(TRIP_TYPE) + factor(DEVICE) +
+              factor(COMPANY) + factor(FAMILY_SIZE),
               data = train,
               family = binomial(link = "logit"))
-summary(model1)
-exp(cbind(odds=coef(model1), confint(model1)))
+summary(model)
+# exp(cbind(odds=coef(model), confint(model)))
 
-prediction <- predict(model1, validation, type="response")
+prediction <- predict(model, validation, type="response")
 
-library(pROC)
 roc_obj <- roc(factor(validation$EXTRA_BAGGAGE), prediction)
 auc(roc_obj)
+
+plot(roc(validation$EXTRA_BAGGAGE, prediction, direction="<"),
+     col="black",
+     xlab="False Positive Rate",
+     ylab="True Positive Rate",
+     main="ROC Curve")
+
+submission = data.frame(predict(model, test, type="response"))
