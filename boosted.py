409bf93d9e7c7d56833023717745f599476fb122
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2018-06-29 20:04:36 +0100

    Added xgboost publication

--- boosted.py
+++ boosted.py
@@ -1,15 +1,15 @@
 """
-================================================
+=============================================================
 
 "All models are wrong, but some are useful."
 George Box, 1976
 
-================================================
+=============================================================
 
-A binary classifier for baggage likelihood
-prediction using gradient boosted machines
+A binary classifier for baggage likelihood prediction using
+gradient boosted machines
 
-================================================
+=============================================================
 """
 
 print(__doc__)
@@ -20,6 +20,7 @@ from matplotlib import pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from xgboost.sklearn import XGBClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from xgboost import plot_importance
 from sklearn.metrics import roc_auc_score
 
@@ -68,17 +69,23 @@ X_test = test[list(features)].values
 
 # Train/validation split to compute F1 and AUC internally without holdout set
 X_train, X_validation, Y_train, Y_validation = train_test_split(X_train,Y_train,
-																test_size=0.33,
-																random_state=747)
+																test_size=0.2,
+																random_state=7)
 
-# Rule-ot-thumb XGBoost parameters
+# Rule-of-thumb XGBoost parameters
 n_estimators=2000
-max_depth=8
+max_depth=12
 learning_rate=0.1
 
-clf = XGBClassifier(n_estimators=n_estimators, max_depth=max_depth,
-					learning_rate=learning_rate)
+clf = XgGBClassifier(n_estimators=n_estimators,
+					  max_depth=max_depth,
+					  learning_rate=learning_rate)
 # clf = XGBClassifier()
+# clf = RandomForestClassifier()
+clf = GradientBoostingClassifier(n_estimators=n_estimators,
+								 max_depth=max_depth,
+								 learning_rate=learning_rate)
+
 clf.fit(X_train, Y_train)
 
 Y_test = clf.predict(X_test)
@@ -86,14 +93,18 @@ Y_test_proba = clf.predict_proba(X_test)
 
 Y_validation_proba = clf.predict_proba(X_validation)
 
-print("--- Model parameters ---")
+print("=====================  Model parameters  =====================")
 print(clf)
 
-print("--- Validation set ---")
-print("AUC;{}".format(roc_auc_score(Y_validation, clf.predict(X_validation))))
+print("=====================  Validation set  =======================")
+print("AUROC = {}".format(roc_auc_score(Y_validation,
+										clf.predict(X_validation))))
 
-print("--- Training set ---")
-print("AUC;{}".format(roc_auc_score(Y_train, clf.predict(X_train))))
+print("======================  Training set  ========================")
+print("AUROC = {}".format(roc_auc_score(Y_train,
+										clf.predict(X_train))))
+
+print("==============================================================")
 
 f = open("submission_xgboost.csv", 'w')
 f.write("ID,EXTRA_BAGGAGE\n")
@@ -109,8 +120,8 @@ array = np.column_stack((Y_validation_proba[:,1], Y_validation))
 
 x = array[np.where(array[:,1] == 1.)]
 y = array[np.where(array[:,1] == 0.)]
-sns.kdeplot(x[:,0], shade=True, label='positives')
-sns.kdeplot(y[:,0], shade=True, label='negatives')
-xlim((0, 1))
+sns.distplot(x[:,0], label='positives')
+sns.distplot(y[:,0], label='negatives')
+plt.xlim(0, 1)
 plt.legend(loc='upper right')
 plt.show()
